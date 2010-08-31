f5e65db18d44a7b09810f936d79ab7d93bb6c78c
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2010-08-31 06:57:11 +0000

    Merge the debianized branch.

--- autogen.sh
+++ autogen.sh
@@ -3,10 +3,4 @@
 set -e
 
 cp -av /usr/share/gettext/gettext.h src/
-autopoint --force
-aclocal -I m4
-autoheader
-libtoolize --automake --copy
-automake --add-missing --copy --foreign --include-deps
-autoconf
-./configure
+autoreconf -ifs
