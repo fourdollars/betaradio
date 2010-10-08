ece26305de6e51d5dac2bbd54c93b36f9ada472a
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2010-10-08 05:08:37 +0000

    Create a script to generate Debian package.

---
+++ mkdeb.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env sh
+
+set -e
+
+if [ ! -e ./configure ]; then
+    ./autogen.sh
+fi
+
+make dist-bzip2
+
+VERSION="$(./configure --version | head -n1 | cut -d ' ' -f 3)"
+
+mv betaradio-${VERSION}.tar.bz2 betaradio_${VERSION}.orig.tar.bz2
+
+tar xf betaradio_${VERSION}.orig.tar.bz2
+
+cp -a ./debian betaradio-${VERSION}
+
+cd betaradio-${VERSION}
+
+case "$*" in
+    ('-S')
+        debuild -S -sa
+        ;;
+    ('-P')
+        pdebuild
+        ;;
+    ('-PS')
+        pdebuild --auto-debsign
+        ;;
+    (*)
+        debuild -us -uc -tc
+        ;;
+esac
+
+cd ..
